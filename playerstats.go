436b8068fdcd4c35af33157fa7f7cfe9391d85bb
Andreas Leeb <andreas.b.leeb@gmail.com>
2018-12-07 17:35:41 +0100

    load user from db and set playerStats

--- playerstats.go
+++ playerstats.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"errors"
+
+	"github.com/vikebot/vbdb"
+)
+
 type playerStats struct {
 	Username string `json:"username"`
 	Kills    int    `json:"kills"`
@@ -11,6 +17,19 @@ type playersStats []playerStats
 // getPlayersStats returns the type playersStats which
 // is a slice of playerStats, it's used for getting
 // information of all players in the game
-func getPlayersStats() (ps playerStats, err error) {
+func getPlayersStats() (ps playersStats, err error) {
+	for _, p := range battle.Players {
+		user, success := vbdb.UserFromID(p.UserID)
+		if !success {
+			return ps, errors.New("unable to load User from db")
+		}
+
+		ps = append(ps, playerStats{
+			Username: user.Username,
+			Kills:    p.Kills,
+			Deaths:   p.Deaths,
+		})
+	}
+
 	return
 }
